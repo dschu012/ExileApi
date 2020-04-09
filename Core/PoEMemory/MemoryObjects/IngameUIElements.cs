d9f90a5c8c6804b6db2ed3a49b7064ce0a064595
danny <dschu012@gmail.com>
2020-04-09 08:34:49 -0400

    chat message offsets for 3.10

--- Core/PoEMemory/MemoryObjects/IngameUIElements.cs
+++ Core/PoEMemory/MemoryObjects/IngameUIElements.cs
@@ -36,7 +36,8 @@ namespace ExileCore.PoEMemory.MemoryObjects
             _DelveWindow ?? (_DelveWindow = GetObject<SubterraneanChart>(IngameUIElementsStruct.DelveWindow));
         public SkillBarElement SkillBar => GetObject<SkillBarElement>(IngameUIElementsStruct.SkillBar);
         public SkillBarElement HiddenSkillBar => GetObject<SkillBarElement>(IngameUIElementsStruct.HiddenSkillBar);
-        public PoeChatElement ChatBox => GetObject<PoeChatElement>(M.Read<long>(Address + 0x3F8, 0x2C8, 0xE20, 0x350));
+        public PoeChatElement ChatBox => GetObject<Element>(M.Read<long>(Address + 0x3F8, 0x2D0)).GetChildAtIndex(2).ReadObjectAt<PoeChatElement>(0x350);
+        public Element ChatMessageBox => GetObject<Element>(M.Read<long>(Address + 0x3F8, 0x300));
         public IList<string> ChatMessages => ChatBox.Children.Select(x => x.Text).ToList();
         public Element QuestTracker => GetObject<Element>(IngameUIElementsStruct.QuestTracker);
         public Element OpenLeftPanel => GetObject<Element>(IngameUIElementsStruct.OpenLeftPanel);
