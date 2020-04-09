c222302e98f4d4f65f1637ee00d8f73386909287
danny <dschu012@gmail.com>
2020-04-09 14:52:24 -0400

    fix certain strings getting missing information

--- Core/PoEMemory/MemoryObjects/NativeStringReader.cs
+++ Core/PoEMemory/MemoryObjects/NativeStringReader.cs
@@ -6,16 +6,21 @@ namespace ExileCore.PoEMemory.MemoryObjects
     {
         public static string ReadString(long address, IMemory M)
         {
-            var Size = M.Read<uint>(address + 0x10);
-            var Capacity = M.Read<uint>(address + 0x18);
+            var Size = M.Read<int>(address + 0x10);
+            var Capacity = M.Read<int>(address + 0x18);
 
+            if(Size < 0)
+            {
+                return string.Empty;
+            }
             //var size = Size;
             //if (size == 0)
             //    return string.Empty;
             if ( /*8 <= size ||*/ 8 <= Capacity) //Have no idea how to deal with this
             {
                 var readAddr = M.Read<long>(address);
-                return M.ReadStringU(readAddr);
+                var output = M.ReadStringU(readAddr, Size * 2, false);
+                return output.Length > Size ? output.Substring(0, Size) : output;
             }
 
             return M.ReadStringU(address);
